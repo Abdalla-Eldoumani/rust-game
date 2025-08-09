0d4becc4cbaf75dd1253b314a44f4861075e9404
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:20:53 -0600

    The file that users will write code in

---
+++ lessons/intro/variables/starter.rs
@@ -0,0 +1,16 @@
+/// Return the number 42 using a variable binding.
+/// - Introduce a binding named `x`
+/// - Optionally use `mut` and reassign
+/// - Return its value
+///
+/// You can compile/test with:
+///   rust-game check --id intro/variables
+///
+/// Also try:
+///   rust-game hint --id intro/variables
+pub fn answer() -> i32 {
+    // TODO: make this return 42
+    0
+}
+
+
