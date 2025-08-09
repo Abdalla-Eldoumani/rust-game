9d68c97aee05a6ab02d183ccea6a672376b29e70
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:32 -0600

    content: add lessons/intermediate/iterators_combinators/starter.rs

---
+++ lessons/intermediate/iterators_combinators/starter.rs
@@ -0,0 +1,11 @@
+/// Parse strings to i32, ignore invalid entries, take first 5 numbers and return their sum.
+pub fn sum_first_five(nums: &[&str]) -> i32 {
+    // TODO: filter_map, take, sum
+    0
+}
+
+
+
+
+
+
