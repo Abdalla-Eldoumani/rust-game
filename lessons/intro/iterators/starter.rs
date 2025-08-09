ee920cc9c0c361e671f44b39510f9fab1c2f0f1b
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:48 -0600

    content: add lessons/intro/iterators/starter.rs

---
+++ lessons/intro/iterators/starter.rs
@@ -0,0 +1,7 @@
+/// Given a slice of i32, compute the sum of squares of even numbers using iterators.
+pub fn sum_squares_of_evens(_nums: &[i32]) -> i32 {
+    // TODO: use iter/filter/map/sum
+    0
+}
+
+
