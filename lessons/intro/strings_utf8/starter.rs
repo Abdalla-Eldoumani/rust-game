e0943419a0b5034eb2a72a40d2d9dac811feabc7
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:58 -0600

    content: add lessons/intro/strings_utf8/starter.rs

---
+++ lessons/intro/strings_utf8/starter.rs
@@ -0,0 +1,11 @@
+/// Return a greeting for the provided name. Use borrowing (&str) and return a new String.
+pub fn greet(name: &str) -> String {
+    // TODO: use format!("Hello, {name}! 🌟")
+    String::new()
+}
+
+
+
+
+
+
