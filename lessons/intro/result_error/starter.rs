ef21c676c776f10a475c836cd0d7674504992747
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:57 -0600

    content: add lessons/intro/result_error/starter.rs

---
+++ lessons/intro/result_error/starter.rs
@@ -0,0 +1,7 @@
+/// Parse the given string to i32. On failure, return Err with the original string.
+pub fn parse_i32(_s: &str) -> Result<i32, String> {
+    // TODO: use .parse() and map_err(|_| s.to_string())
+    Ok(0)
+}
+
+
