f16c39247e13c233f9a296221976629552858518
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:50 -0600

    content: add lessons/intro/match_enums/starter.rs

---
+++ lessons/intro/match_enums/starter.rs
@@ -0,0 +1,12 @@
+pub enum Shape {
+    Circle(f32),
+    Square(f32),
+}
+
+/// Return the area of the shape. Use `match` and cover all variants.
+pub fn area(_s: Shape) -> f32 {
+    // TODO
+    0.0
+}
+
+
