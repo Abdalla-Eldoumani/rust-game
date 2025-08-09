86d7cebfcbadefc090bd9f0cf3605028f339024f
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:44 -0600

    content: add lessons/intro/functions/starter.rs

---
+++ lessons/intro/functions/starter.rs
@@ -0,0 +1,10 @@
+/// Implement `add` to return the sum of two `i32`.
+///
+/// You can test via GUI Check or CLI:
+///   cargo run -- check --id intro/functions
+pub fn add(_a: i32, _b: i32) -> i32 {
+    // TODO
+    0
+}
+
+
