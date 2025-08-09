c2b4402fb7a4d5f9f66bef4d7a1030e7bebfd9f7
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 17:40:40 -0600

    content: add lessons/intro/arrays_slices_basics/starter.rs

---
+++ lessons/intro/arrays_slices_basics/starter.rs
@@ -0,0 +1,7 @@
+/// Return the sum of the first n elements of the slice (or all if n > len).
+pub fn sum_first(slice: &[i32], n: usize) -> i32 {
+    // TODO: cap n at slice.len() and sum
+    0
+}
+
+
